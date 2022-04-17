5b56909fc1c6a92e68141ee3a554b8b75667349e
Umut Akpinar <umtakpnr5@gmail.com>
2022-04-17 20:28:11 +0300

    (panel editelenecek)

--- website/admin/panel.php
+++ website/admin/panel.php
@@ -14,24 +14,26 @@ if (session("login") == false) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin Panel</title>
+    <!-- Bootstrap -->
+    <!-- <link rel="stylesheet" href="./website/plugins/bootstrap/bootstrap.min.css"> -->
+    <link rel="stylesheet" href="../plugins/bootstrap/bootstrap.min.css" />
+    <!-- FontAwesome -->
+    <link rel="stylesheet" href="../plugins/fontawesome/css/all.min.css">
+    <!-- Animation -->
+    <link rel="stylesheet" href="../plugins/animate-css/animate.css">
+    <!-- slick Carousel -->
+    <link rel="stylesheet" href="../plugins/slick/slick.css">
+    <link rel="stylesheet" href="../plugins/slick/slick-theme.css">
+    <!-- Colorbox -->
+    <link rel="stylesheet" href="../plugins/colorbox/colorbox.css">
+    <!-- Template styles-->
+    <link rel="stylesheet" href="../css/style.css">
+
 </head>
 
-<!-- Bootstrap -->
-<!-- <link rel="stylesheet" href="./website/plugins/bootstrap/bootstrap.min.css"> -->
-<link rel="stylesheet" href="../plugins/bootstrap/bootstrap.min.css" />
-<!-- FontAwesome -->
-<link rel="stylesheet" href="../plugins/fontawesome/css/all.min.css">
-<!-- Animation -->
-<link rel="stylesheet" href="../plugins/animate-css/animate.css">
-<!-- slick Carousel -->
-<link rel="stylesheet" href="../plugins/slick/slick.css">
-<link rel="stylesheet" href="../plugins/slick/slick-theme.css">
-<!-- Colorbox -->
-<link rel="stylesheet" href="../plugins/colorbox/colorbox.css">
-<!-- Template styles-->
-<link rel="stylesheet" href="../css/style.css">
 
 <body>
+    <!-- PANEL EDİTLENECEK SEKMELER OLUŞTURULACAK -->
     <div class="d-flex">
         <div id="header" class="row container bg-warning">
             header
